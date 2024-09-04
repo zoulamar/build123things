381d11ab799d6d2137d46690ed27bdf3c61d3944
zoulamar@cras-01 <zoulamar@cras-01>
2024-09-04 14:07:02 +0200

    feat: utility environment module

---
+++ build123things/env.py
@@ -0,0 +1,15 @@
+"""
+Module contains convenient aliases and utilities for daily usage.
+"""
+
+import build123d
+from build123things import MountPoint, Thing
+
+bd = build123d
+
+MIN=bd.Align.MIN
+MAX=bd.Align.MAX
+CEN=bd.Align.CENTER
+
+L = build123d.Location
+M = MountPoint
